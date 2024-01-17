c90b44f7f6fd7be172d2019cae1e8af70f62b3cf
Lorenzo <lorenzonicotera02@gmail.com>
2024-01-17 23:27:55 +0100

    fixed some stuff on cpp01 ex00

--- cpp01/ex00/randomChump.cpp
+++ cpp01/ex00/randomChump.cpp
@@ -6,7 +6,7 @@
 /*   By: lnicoter <lnicoter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 18:08:15 by lnicoter          #+#    #+#             */
-/*   Updated: 2023/12/28 18:08:24 by lnicoter         ###   ########.fr       */
+/*   Updated: 2024/01/17 23:26:44 by lnicoter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 void randomChump(std::string name)
 {
-	Zombie *randoZombie = newZombie(name);
-	randoZombie->announce();
-	delete randoZombie;
+	Zombie randoZombie(name);
+	randoZombie.announce();
 }
