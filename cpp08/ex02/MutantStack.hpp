a6ca0ef6621297c10ebf2f90f300b2b68f58b886
Lorenzo <lorenzonicotera02@gmail.com>
2024-03-23 14:34:00 +0100

    cpp08 finished

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,33 +6,63 @@
 /*   By: lnicoter <lnicoter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 17:03:33 by lnicoter          #+#    #+#             */
-/*   Updated: 2024/03/19 20:20:03 by lnicoter         ###   ########.fr       */
+/*   Updated: 2024/03/22 19:03:35 by lnicoter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
+#include <iostream>
 #include <stack>
 #include <iterator>
+#include <vector>
 #include <algorithm>
-
+#include <deque>
 /*
 per risolvere il dilemma dell'iterazione pare
 ci sia bisogno dell'ereditarietà
 */
-template <typename T>
+template <typename T, typename Container = std::deque<T> >
 
-class MutantStack
+class MutantStack : public std::stack<T, Container>
 {
-	private:
-		std::stack<T> noIterate;
+	// private:
+		// std::stack<T> noIterate;
 	public:
-		MutantStack();
-		~MutantStack();
-		MutantStack(const MutantStack& obj);
-		MutantStack& operator=(const MutantStack& obj);
+		/*mi segno anche le cose che voglio capire meglio per veramente
+		sapere che cosa "ho fatto" nell'esercizio*/
+		// class Iterator : public std::iterator<std::forward_iterator_tag, T>
+		// {
+		// 	private:
+		// 		typename std::stack<T>::containert_type
+		// }
 
+		MutantStack(){}
+		~MutantStack(){}
+		MutantStack(const MutantStack& obj)
+		{
+			if (this != obj)
+				*this = obj;
+		}
+
+		MutantStack& operator=(const MutantStack& obj)
+		{
+			std::stack<T, Container>::operator=(obj);
+			return *this;
+		}
+
+		typedef typename Container::iterator iterator;
+
+		iterator	begin()
+		{
+			return (this->c.begin());
+		}
+
+		iterator end()
+		{
+			return (this->c.end());
+		}
 		//methods of the class
 
 };
