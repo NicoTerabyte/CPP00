09fe083de810220c4298b0ba9848ed63b752d8c6
Lorenzo Nicotera <lnicoter@e3r6p3.42roma.it>
2024-04-29 18:19:30 +0200

    added some explanation for cpp06 all ok btw

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -6,11 +6,18 @@
 /*   By: lnicoter <lnicoter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 15:55:41 by lnicoter          #+#    #+#             */
-/*   Updated: 2024/03/05 17:04:11 by lnicoter         ###   ########.fr       */
+/*   Updated: 2024/04/29 18:16:02 by lnicoter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.h"
+
+/*
+used to convert a pointer of some data type
+into a pointer of another data type,
+even if the data types
+before and after conversion are different
+*/
 
 int	main(void)
 {
