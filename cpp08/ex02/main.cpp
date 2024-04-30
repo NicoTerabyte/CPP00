3765dd76c01d5a8758032d332673e5c1be18dc96
Lorenzo <lorenzonicotera02@gmail.com>
2024-04-30 02:22:10 +0200

    made some refinement to cpp08 for extra points but something is off with the list implementation of the last excercise

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lnicoter <lnicoter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 15:23:57 by lnicoter          #+#    #+#             */
-/*   Updated: 2024/03/23 15:21:47 by lnicoter         ###   ########.fr       */
+/*   Updated: 2024/04/30 02:20:52 by lnicoter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int main()
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
+
 	mstack.push(0);
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
@@ -35,5 +35,28 @@ int main()
 		++it;
 	}
 	std::stack<int> s(mstack);
+	std::cout<<"\nNow we are doing the List version\n"<<std::endl;
+	//list version to prove similarity with the mutant stack
+	std::list<int>	listVersion;
+	listVersion.push_front(5);
+	listVersion.push_front(17);
+	std::cout<<listVersion.front()<<std::endl;
+	listVersion.pop_front();
+	std::cout<<listVersion.size()<<std::endl;
+	listVersion.push_back(3);
+	listVersion.push_back(5);
+	listVersion.push_back(737);
+	listVersion.push_back(0);
+
+	std::list<int>::iterator	itList = listVersion.begin();
+	std::list<int>::iterator	iteList = listVersion.end();
+
+	++itList;
+	--itList;
+	while (itList != iteList)
+	{
+		std::cout<< *itList <<std::endl;
+		itList++;
+	}
 	return 0;
 }
